73e73c4330f85f3c25a5ae1a6051dd7c04cdb0df
Famondir <schaefersd@gmail.com>
2024-12-19 19:29:27 +0100

    serving intex.html via static file

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
+# from flask_socketio import SocketIO, join_room, leave_room, emit
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates', static_url_path='/')
 
 
 @app.route('/', methods=['GET'])
-def hello():
-    return render_template('index.html')
-
-@app.route('/python-2', methods=['POST'])
-def python2():
-    return render_template('python2.html')
-
-@app.route('/python-3', methods=['POST'])
-def python3():
-    return render_template('python3.html')
+def index():
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
